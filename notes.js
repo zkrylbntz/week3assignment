3fa0c3ff140e194312754d27cc6912e8f5f9fce6
Zak Le Bentz <zakerylebentz@gmail.com>
2024-09-14 23:49:54 +0100

    Second push, information pulled, some stuff in place, Cookie counter and buttons attempted

---
+++ notes.js
@@ -0,0 +1,111 @@
+console.log("testline1");
+
+//we need to store two global values: cookie count and cookies per second
+
+//variable
+let cookieCount = 0;
+let cookiesPerSecond = 0;
+
+// //object
+// // if you choose the stats object, you don't need the global variables
+// let stats = {
+//   cookieCount: 0,
+//   cookiesPerSecond: 0,
+// };
+
+//dom manipulation
+//selecting dom elements (buttons, imgs, p tags etc....)
+const countCookies = document.getElementById("count");
+// a way to store the shop upgrades that come from the API
+
+// let shopUpgrades = [];
+
+// fetch the items from the API
+// async function getShopUpgrades() {
+//   const response = await fetch(
+//     "https://cookie-upgrade-api.vercel.app/api/upgrades"
+//   );
+//   console.log(respose);
+//   const date = await response.json();
+//   console.log(data);
+//   return data;
+//   //fetch the data
+//   //translate into JSON
+//   //PUSH the items into the shopUpgrades array above
+// }
+
+// getShopUpgrades();
+
+// const shopContainer = document.getElementById("shop-container");
+
+// function shopUpgrades(stringContent){
+//   const content = document.createElement ("p");
+//   content.textContent = stringContnet;
+//   shopContainer.appendChild(content);
+// }
+
+// async function displayUpgrades()[
+//   const postsUpgrades = await getPo
+// ]
+
+//an event listener to click on the cookie button
+//make sure cookie or button/img is selected
+//write event handler and event listener
+function handleCookieClick() {
+  // when I click on the cookie, the value of cookieCount goes up by one
+}
+addEventListener("click", handleCookieClick);
+
+// we need a timer that increases the cookiesCount value by one every second
+setInterval(function () {
+  //I want to increase the value of cookieCount by one every second
+  //I want to update the value displayed on the page (this task can also be a seperate function, for example updateCookieCount(), and you would call this funcion inside the setInterval function)
+  //I want to store this value in local storage
+}, 1000);
+
+// function updateCookieCount() {
+//   //I update the cookieCount value (this is just one option)
+// }
+
+function storeLocalStorage() {
+  //I store the values in locak storage (this is just one option)
+}
+
+//==============================================
+//extra function blocks to give you other ideas
+// these building block are just possible solutions to problems you might find
+function renderShopUpgrades() {
+  //create DOM elements
+  //you will use a loop or array method
+  shopUpgrades.forEach(function (upgrade) {
+    //for each item in the array, assign the value to a dom element, append the element to the dom
+  });
+  //}
+
+  function saveLocalStorage() {
+    // a method that turns your data into string soup
+    // a method to set the data into key and values in local storage
+  }
+}
+
+myButton.addEventListener("click", handleClick);
+
+//counting button
+const myCountingButton = document.getElementById("count-button");
+console.log(myCountingButton);
+
+let clicks = 0;
+
+function handleCounting() {
+  clicks = clicks + 1;
+  console.log(clicks);
+  if (clicks < 10) {
+    myCountingButton.textContent = `You have clicked ${clicks} times!`;
+  } else if (clicks > 9 && clicks < 20) {
+    myCountingButton.textContent = `You can stop now... ${clicks}`;
+  } else {
+    myCountingButton.textContent = `Okay..I get it... ${clicks}`;
+  }
+}
+
+myCountingButton.addEventListener("click", handleCounting);
--- script.js
+++ script.js
@@ -1,1 +1,100 @@
 console.log("testline1");
+
+let cookieCount = 0;
+let cookiesPerSecond = 0;
+// let addCookiesPerSecond = 0;
+
+const mainClicker = document.getElementById("main-button");
+const resetButton = document.getElementById("reset-button");
+const countCookies = document.getElementById("count");
+const increaseCPS = document.getElementById("per-second");
+const shopContainer = document.getElementById("shop-container");
+const idContainer = document.getElementById("id-container");
+const nameContainer = document.getElementById("name-container");
+const costContainer = document.getElementById("cost-container");
+const increaseContainer = document.getElementById("increase-container");
+const upgrade1 = document.getElementById("1");
+
+let shopUpgrades = [];
+
+async function getShopUpgrades() {
+  const response = await fetch(
+    "https://cookie-upgrade-api.vercel.app/api/upgrades"
+  );
+  console.log(response);
+  const data = await response.json();
+  console.log(data);
+  return data;
+}
+
+getShopUpgrades();
+
+async function displayUpgrades() {
+  const postsUpgrades = await getShopUpgrades();
+  postsUpgrades.forEach(function (post) {
+    document.createElement("p");
+    let id = document.createElement("p");
+    let name = document.createElement("p");
+    let cost = document.createElement("p");
+    let increase = document.createElement("p");
+    id.className = "class-id";
+    name.className = "class-name";
+    cost.className = "class-cost";
+    increase.className = "class-increase";
+    id.textContent = post.id;
+    name.textContent = post.name;
+    cost.textContent = post.cost;
+    increase.textContent = post.increase;
+    idContainer.appendChild(id);
+    nameContainer.appendChild(name);
+    costContainer.appendChild(cost);
+    increaseContainer.appendChild(increase);
+  });
+}
+
+displayUpgrades();
+
+mainClicker.addEventListener("click", handleCookieClick);
+
+function handleCookieClick() {
+  cookieCount = cookieCount + 1;
+  countCookies.textContent = `Cookie count :- ${cookieCount}`;
+  cookiesPerSecond = 1;
+  increaseCPS.textContent = `Cookies per second :- ${cookiesPerSecond}`;
+
+  setInterval(function (addCookiesPerSecond) {
+    cookiesPerSecond = cookiesPerSecond + 1;
+    countCookies.textContent = `Cookie count :- ${cookiesPerSecond}`;
+  }, 1000);
+}
+
+// setInterval(function (addCookiesPerSecond) {
+//   cookiesPerSecond = cookiesPerSecond + 1;
+//   countCookies.textContent = `Cookie count :- ${cookiesPerSecond}`;
+// }, 1000);
+
+resetButton.addEventListener("click", handleResetClick);
+
+function handleResetClick() {
+  cookieCount = 0;
+  cookiesPerSecond = 0;
+  countCookies.textContent = `Cookie count :- ${cookieCount}`;
+  increaseCPS.textContent = `Cookies per second :- ${cookieCount}`;
+  // stopInterval(addCookiesPerSecond);
+}
+
+//cookie button adds amount of cookies in cookies persecond
+//cookies = 0 + 1 etc
+//as well as adding that amount per second
+//cps= 0 , 1, 2, 3, 4 etc
+//upgrades need to update cps and cookies, but be a running total
+//upgrade 5 - 10, 15, 20 ,25
+// or adding on to whatever the number thats added
+//also take away that amount from cookies first, before adding on to click
+
+upgrade1.addEventListener("click", upgrade1Click);
+
+function upgrade1Click() {
+  cookieCount - 5;
+  cookiesPerSecond + 5;
+}
